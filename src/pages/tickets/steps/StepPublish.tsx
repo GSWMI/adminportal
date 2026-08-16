46eef1f27bf72eae51e04ee1681b3067a064afff
moyinfesobi <moyinoluwafesobi7@gmail.com>
2026-08-16 15:49:18 +0100

    chire: changed base URL

--- src/pages/tickets/steps/StepPublish.tsx
+++ src/pages/tickets/steps/StepPublish.tsx
@@ -10,7 +10,7 @@ interface Props {
   onPreview: () => void
 }
 
-const PUBLIC_BASE_URL = 'https://gswmi-attendee.netlify.app'
+const PUBLIC_BASE_URL = 'https://gswmi.net'
 
 export default function StepPublish({ onPreview }: Props) {
   const [published, setPublished] = useState(false)
