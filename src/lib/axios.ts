f1e27508325206b09cba3c2f7d265a5b1cfedc0a
moyinfesobi <moyinoluwafesobi7@gmail.com>
2026-08-19 21:42:37 +0100

    chore: changed base url

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const BASE_URL = import.meta.env.VITE_API_BASE_URL ?? 'https://gwmi-backend-staging.onrender.com/api' 
+const BASE_URL = import.meta.env.VITE_API_BASE_URL ?? 'https://gswmi-backend-prod.onrender.com' 
 
 const api = axios.create({
   baseURL: BASE_URL,
